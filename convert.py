dc81fe353d7940214030b96cc33843802e61044a
Gabriel Lima Dantas <101371170+PeDeLaranjaLima@users.noreply.github.com>
2025-12-04 20:23:29 -0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -9,27 +9,3 @@ class convert:
         self.VOCtoYOLO = VOCtoYOLO
         self.COCOtoYOLO = COCOtoYOLO
         self.YOLOtoCOCO = YOLOtoCOCO
-
-"""
-
-EXEPLO DE USO 
-
-    # Caminho base pode ser relativo ou absoluto
-    base_dir = Path("CPLID_val") / "val"
-
-    images_dir = base_dir / "images"
-    labels_dir = base_dir / "labels"
-    output_file = Path("saida.json")
-
-    conv = convert()
-
-    coco = conv.YOLOtoCOCOConverter(
-        images_dir,
-        labels_dir,
-        ["013"],
-        output_file,
-    )
-
-    coco.convert()
-
-"""
